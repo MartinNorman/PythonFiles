5374bbf243fd831fd9c7ca5e6dcd62a0509ac513
Martin Norman <martin.norman@climber.se>
2019-08-26 18:38:10 +0200

    Added error handling when 500 reply and local file with paths

--- Avanza_GetNav.py
+++ Avanza_GetNav.py
@@ -1,30 +1,38 @@
 ﻿import requests
 import codecs
-import json
 import datetime
-
+from local_paths import output
+from local_paths import output_error
+from local_paths import input
 vDate = datetime.datetime.now().strftime("%Y-%m-%d")
-print(vDate)
 
-#Funds
-Nav = codecs.open(r'/volume1/homes/Martin/Avanza/Result/Nav' + vDate + '.txt', 'w', 'utf-8')
+# Funds
+Nav = codecs.open(output + '/' + 'Nav' + vDate + '.txt', 'w', 'utf-8')
 texttowrite = 'ID@ISIN@NAV@NAVDATE'
 Nav.write(texttowrite + '\r\n')
-print("File to write to created")
 
-#Get IDs to read
-#f = open('/volume1/homes/Martin/Avanza/ID.txt', mode='r')
-print("File to read from opened")
-f = open('/volume1/homes/Martin/Avanza/ID.txt', mode='r', encoding = 'utf-8')
+# Funds with errors
+ErrorID = codecs.open(output_error + '/' + 'Error' + vDate + '.txt', 'w', 'utf-8')
+texttowrite = 'ID@ERROR'
+ErrorID.write(texttowrite + '\r\n')
+
+# Get IDs to read
+f = open(input + '/' + 'ID.txt', mode='r', encoding='utf-8')
 for x in f:
-	print(x)
-	static = "https://www.avanza.se/_cqbe/fund/guide/"
-	res = requests.get(static + x.strip())
-	x_stripped = x.strip()
-	res2 = res.json()
-	isin = res2["isin"]
-	nav = res2["nav"]
-	navDate = res2["navDate"]
-	
-	#Write to file
-	Nav.write("%s" %x_stripped + "@" + "%s" %isin + "@" + "%s" %nav + "@" + "%s" %navDate + '\r\n')  
+    print(x)
+    static = "https://www.avanza.se/_cqbe/fund/guide/"
+    res = requests.get(static + x.strip())
+    x_stripped = x.strip()
+    res2 = res.json()
+    print(res2)
+    try:
+        isin = res2["isin"]
+        print(isin)
+        nav = res2["nav"]
+        print(nav)
+        navDate = res2["navDate"]
+        print(navDate)
+        # Write to file
+        Nav.write("%s" % x_stripped + "@" + "%s" % isin + "@" + "%s" % nav + "@" + "%s" % navDate + '\r\n')
+    except Exception as e:
+        ErrorID.write("%s" % x_stripped  + "@" + "%s" % str(e) + '\r\n')
